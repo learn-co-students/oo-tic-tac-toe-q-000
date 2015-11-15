7442bddd7615a9bc6cdbc9dab8ee87268bd60d9b
shmulim <shmuel.markel@gmail.com>
2015-11-14 23:19:51 -0800

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,4 @@
 class TicTacToe
-
   WIN_COMBINATIONS = [
     [0, 1, 2],
     [6, 7, 8],
@@ -27,27 +26,61 @@ class TicTacToe
     end
   end
 
-  def turn
-  puts "Please enter 1-9:"
-  @pos = gets.strip
-
-  # TODO: implement recursion
-  until valid_move?(@pos)
-    puts "Please enter 1-9:"
-    @pos = gets.strip
+  def won?
+    WIN_COMBINATIONS.find do |win_combo|
+      x_wins = win_combo.all? { |pos| @board[pos] == "X" }
+      o_wins = win_combo.all? { |pos| @board[pos] == "O" }
+      win_combo if x_wins || o_wins
+    end
   end
 
-  move(@pos, current_player)
-  display_board  
-end
+  def over?
+    draw? || won?
+  end
 
-def current_player    
-  turn_count % 2 == 0 ? "X" : "O"
-end
+  def draw?
+    full? && !won?
+  end
 
-def move(pos, player = "X")
-  @board[pos.to_i-1] = player
-end
+  def full?
+    @board.all? { |pos| pos == "X" || pos == "O" }
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    @pos = gets.strip
+
+    # TODO: implement recursion
+    until valid_move?(@pos)
+      puts "Please enter 1-9:"
+      @pos = gets.strip
+    end
+
+    move(@pos, current_player)
+    display_board  
+  end
+
+  def valid_move?(pos)
+    pos.to_i.between?(1,9) && !position_taken?(pos.to_i-1)
+  end
+
+  def position_taken?(pos)
+    @board[pos.to_i] != " " && @board[pos.to_i] != ""
+  end
+
+  def move(pos, player = "X")
+    @board[pos.to_i-1] = player
+  end
+
+  def current_player    
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+
+  def turn_count
+    count = 0
+    @board.each { |pos| count +=1 if pos == "X" || pos == "O" }
+    count
+  end
 
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
@@ -57,43 +90,7 @@ end
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-
-  def won?
-    WIN_COMBINATIONS.find do |win_combo|
-      x_wins = win_combo.all? { |pos| @board[pos] == "X" }
-      o_wins = win_combo.all? { |pos| @board[pos] == "O" }
-      win_combo if x_wins || o_wins
-    end
-  end
-
-  def position_taken?(pos)
-    @board[pos.to_i] != " " && @board[pos.to_i] != "" # removed decrement
-  end
-
-  def valid_move?(pos)
-    pos.to_i.between?(1,9) && !position_taken?(pos.to_i-1)
-  end
-
   def winner
     won? ? @board[won?[0]] : nil
   end
-
-  def full?
-    @board.all? { |pos| pos == "X" || pos == "O" }
-  end
-
-  def draw?
-    full? && !won?
-  end
-
-  def over?
-    draw? || won?
-  end
-
-  def turn_count
-    count = 0
-    @board.each { |pos| count +=1 if pos == "X" || pos == "O" }
-    count
-  end
-
 end
