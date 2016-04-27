488c82a3bbbbdcaedc4dfe4c89445af04520c4ce
achasveachas <chilikkal@gmail.com>
2016-04-27 03:09:02 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,17 +53,10 @@ class TicTacToe
     end
   end
   def won?
-    WIN_COMBINATIONS.any? do |win_combination|  
-      win_index_1 = win_combination[0]
-      win_index_2 = win_combination[1]
-      win_index_3 = win_combination[2]
- 
-      position_1 = @board[win_index_1] 
-      position_2 = @board[win_index_2] 
-      position_3 = @board[win_index_3]  
-      if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")      
-        return win_combination  
-      end
+    WIN_COMBINATIONS.detect do |combo|
+      @board[combo[0]] == @board[combo[1]] &&
+      @board[combo[1]] == @board[combo[2]] &&
+      position_taken?(combo[0])
     end
   end
   def full?
