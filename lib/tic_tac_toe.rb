09ec830d7b56d6e471fb006dd0dd2b744a2aab2b
conuska <onuskacc@gmail.com>
2016-04-28 22:45:44 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,44 @@
+WIN_COMBINATIONS=
+  [ [0,1,2] ,
+[3,4,5] ,
+[6,7,8] ,
+[0,3,6] ,
+[1,4,7] ,
+[2,5,8] ,
+[0,4,8] ,
+[6,4,2] ]
+
+class TicTacToe
+
+
+  def initalize(board)
+    @board = [" ", " "," ", " "," ", " "," "]
+    @board = board
+  end
+
+#   def display_board
+#   end
+#   def move
+#   end
+#   def position_taken?
+#   end
+#   def valid_move?
+#   end
+#   def turn
+#   end
+#   def turn_count
+#   end
+#   def current_player
+#   end
+#   def won?
+#   end
+#   def full?
+#   end
+#   def draw?
+#   end
+#   def over?
+#   end
+#   def play
+#   end
+
+end
