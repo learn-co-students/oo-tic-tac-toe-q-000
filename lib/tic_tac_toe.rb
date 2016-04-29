066bff0656c56cb2e3f42a99af7249923fe922c7
Sandy <SVern99@aol.com>
2016-04-29 22:48:10 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,18 +27,113 @@ class TicTacToe
     @board
   end
 
-  def move(board, location, char="X")
+  def board=(new_board)
+    @board=new_board
+  end
+
+  def move(location, char="X")
     @board[location.to_i-1] = char
   end
 
-=begin
-  def current_player
-    turn_count(@board).even? ? "X" : "O"
+  def position(location)
+    @board[location.to_i]
+  end
+
+  def position_taken?(location)
+    !(@board[location] == " " || @board[location] == "" || @board[location] == nil)
+  end
+
+  def valid_move?(location)
+    location.to_i.between?(1,9) && !position_taken?(location.to_i - 1)
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    location = gets.strip
+  while valid_move?(location) != true
+     puts "Please enter 1-9:"
+     location = gets.strip
+  end
+    move(location, current_player)
+    display_board
   end
 
   def turn_count
-    @board.count{|token| char == "X" || char == "O"}
+   counter = 0
+   board.each do |turn|
+     if turn == "X"
+       counter += 1
+     elsif turn == "O"
+       counter += 1
+     else turn == " "
+       counter += 0
+     end
+   end
+     counter
+end
+
+  def current_player
+    turn_count.even? ? "X" : "O"
   end
-=end
+
+  def won?
+    result = false
+    WIN_COMBINATIONS.each do |win_combination|
+      if !result
+        if win_combination.all?{|i| @board[i]=="X"} || win_combination.all?{|i| @board[i]=="O"}
+           result = win_combination
+        end
+      end
+    end
+     result
+  end
+
+  def full?
+    if @board.any?{|location| location == " "}
+      false
+    else
+      true
+    end
+  end
+
+  def draw?
+    if won? == false && full? == true
+      true
+    else
+     false
+    end
+  end
+
+  def over?
+    if won? == false && draw? == false && full? == false
+      false
+    else
+      true
+    end
+  end
+
+  def winner
+    win_combination = won?
+      if win_combination
+        @board[win_combination[0]]
+      else
+        nil
+      end
+  end
+
+  def play
+    while !over?
+      turn
+  end
+
+   if draw?
+      puts "Cats Game!"
+   end
+
+   if won?
+      puts "Congratulations #{winner}!"
+   end
+
+end
 
 end
