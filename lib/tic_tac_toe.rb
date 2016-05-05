37e91ad8caf0a94b4cf2eaba912b13cfc77e9454
sam oteo <tortillabar@me.com>
2016-05-04 19:36:55 -0600

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,10 +27,107 @@ def display_board
 end
 
 #MOVE
-def move(board, position, character = "X") #used to be def move(board, position, character = "X")
-  board [position.to_i - 1] = character
+def move(location, character = "X")
+  @board[location.to_i - 1] = character
 end
 
+#position_taken
+def position_taken?(position)
+   if @board[position] == " "|| @board[position] == "" || @board[position] == nil
+    false
+  elsif @board[position] == "X" || @board[position] == "O"
+    true
+  end
+end
 
+#Valid_move
+def valid_move?(position)
+  position.to_i.between?(1,9) && !position_taken?(position.to_i-1)
+end
+
+#TURN
+def turn              #get input
+  puts "Please enter 1-9:"
+  input = gets
+  if valid_move?(input) #validate the move #used to be
+    move(input)
+    else
+turn
+  end
+  display_board
+end
+
+#TURN_COUNT
+def turn_count  #defines the #turn_count method
+  counter = 0  #sets counter or count of turns to 0
+  @board.each do |position| #implementing iterator #each to loop over the elements of the board array
+    if position == "X" || position == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+#CURRENT_PLAYER
+def current_player
+
+  if turn_count.odd?
+    "O"
+  else turn_count.even?
+    "X"
+  end
+end
+
+#WONwin
+def won?
+  WIN_COMBINATIONS.each do| position |
+    if @board[position[0]] == "X" && @board[position[1]] == "X" && @board[position[2]] == "X" || @board[position[0]] == "O" && @board[position[1]] == "O" && @board[position[2]] == "O"
+      return position
+    end
+  end
+  false
+end
+
+#FULL
+def full?
+  @board.all? {|position| position == "X" || position == "O"} #all board is full with x or o
+ # return true if every element in the board contains either an "X" or an "O"
+end
+
+#DRAW
+def draw?
+  if !won? && full?
+    true
+   else
+      false
+  end
+end
+
+#OVER
+def over?
+  if full? && won? || draw?
+    true
+  end
+end
+
+#WINNER
+def winner
+  if won? == false
+    nil
+  else @board[won?[0]]
+  end
+end
+
+#Play
+def play
+  while !over?
+    turn
+  end
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "A draw!"
+end
+end
 
 end
