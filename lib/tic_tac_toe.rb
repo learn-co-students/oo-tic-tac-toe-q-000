64d503c12a592fc7d6e961c6c02079111f23f9e9
Iliass sabillah <iliass24@gmail.com>
2016-05-12 15:00:19 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,3 @@
-
 class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
@@ -12,7 +11,7 @@ class TicTacToe
     [2,5,8], # Right column
     [0,4,8], # Right diagonal
     [2,4,6]  # Left diagonal
-  ]
+    ]
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
   end
@@ -20,7 +19,6 @@ class TicTacToe
   def turn_count
     @board.count{|token| token == "X" || token == "O"}
   end
-
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
@@ -29,98 +27,90 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
   def position_taken?(location)
-  location=location.to_i
-  @board[location] != " " && @board[location] != ""
+    location=location.to_i
+    @board[location] != " " && @board[location] != ""
   end
   def move(location, token="X" )
-  @board[location.to_i - 1] = token
+    @board[location.to_i - 1] = token
   end
- def valid_move?(position)
+  def valid_move?(position)
     position = position.to_i - 1
-     if position_taken?(position) == false && position.to_i.between?(0, 8)
+    if position_taken?(position) == false && position.to_i.between?(0, 8)
       return true
     elsif position_taken?(position) == true
       return false
     else
       return false
     end
-   end
+  end
   def turn
-      puts "Please enter 1-9:"
+    puts "Please enter 1-9:"
+    position = gets.strip
+    if valid_move?(position) == true
+      move(position, token = current_player)
+    elsif
+    until valid_move?(position) == true
+      puts "Invalid move.  Please select an unoccupied position on the board."
       position = gets.strip
-      if valid_move?(position) == true
-        move(position, token = current_player)
-       elsif
-        until valid_move?(position) == true
-        puts "Invalid move.  Please select an unoccupied position on the board."
-        position = gets.strip
-         end
+    end
+    end
+    won?
+    display_board
+  end
+  def won?
+    WIN_COMBINATIONS.each do |win_combination|
+      win_index1= win_combination[0]
+      win_index2= win_combination[1]
+      win_index3= win_combination[2]
+      pos_1 = @board[win_index1]
+      pos_2 = @board[win_index2]
+      pos_3 = @board[win_index3]
+      if (pos_1 == "X" && pos_2 == "X" && pos_3 == "X")
+        return win_combination
       end
-      won?
-      display_board
-
+      if  (pos_1 == "O" && pos_2 == "O" && pos_3 == "O")
+        @win_var=win_combination
+        return win_combination
+      end
     end
-def won?
-  @win_var=[]
-  WIN_COMBINATIONS.each do |win_combination|
-		win_index1= win_combination[0]
-		win_index2= win_combination[1]
-		win_index3= win_combination[2]
-		pos_1 = @board[win_index1]
-		pos_2 = @board[win_index2]
-		pos_3 = @board[win_index3]
-
-	     if (pos_1 == "X" && pos_2 == "X" && pos_3 == "X")
-	        return win_combination
-		    end
-        if  (pos_1 == "O" && pos_2 == "O" && pos_3 == "O")
-	         @win_var=win_combination
-          return win_combination
-	      end
-	 	  end
-       return false
-    end
-
-##
-
+    return false
+  end
+  ##
   def full?
-  !@board.any? {|element| (element ==" ") || (element =="")}
-end
-def draw?
-  if !won? && full?
-    return true
-  elsif !won?&&!full?
-    return false
-  elsif won?
-    return false
+    !@board.any? {|element| (element ==" ") || (element =="")}
   end
-end
-def over?
-  if won?||full?||draw?
-    return true
-  end
-end
-def winner
-	  if win_combination =won?
-
-	    @board[win_combination.first]
+  def draw?
+    if !won? && full?
+      return true
+    elsif !won?&&!full?
+      return false
+    elsif won?
+      return false
     end
+  end
+  def over?
+    if won?||full?||draw?
+      return true
+    end
+  end
+  def winner
+      if win_combination =won?
 
-end
- # Define your play method below
-def play
-	until over?
-		turn
+        @board[win_combination.first]
+      end
+  end
+   # Define your play method below
+  def play
+    until over?
+    turn
     end
     if won?
-        puts "Congratulations #{winner}!"
-       elsif draw?
-        puts "Cats Game!"
-      else
-      end
-
-end
-
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cats Game!"
+    else
+    end
+  end
 end
 
 
