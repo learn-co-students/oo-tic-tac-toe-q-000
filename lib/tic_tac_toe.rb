8a8bf47e5d9ad1fb6e15ad5ea5c6990d3c02848f
Tyler Taylor <tylerdottaylor@gmail.com>
2016-06-27 18:32:34 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,9 +22,9 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  def move(index, token="X")
-    #index = index.to_i - 1
-    @board[index] = token
+  def move(index, current_player)
+    index = index.to_i - 1
+    @board[index] = current_player
   end
 
   def position_taken?(index)
@@ -32,19 +32,106 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    #@index = index.to_i - 1
+    index = index.to_i - 1
     index.between?(0,8) && !position_taken?(index)
   end
 
   def turn
     puts "Please enter 1-9:"
     input = gets.strip
-    index = input.to_i - 1
-    if valid_move?(index)
-      move(index)
+    #index = input.to_i - 1
+    if valid_move?(input)
+      move(input, current_player)
       display_board
     else
       turn
     end
   end
+
+  def turn_count
+    turn_count = 0
+    @board.each do |turn|
+      if turn == "X" || turn == "O"
+        turn_count += 1
+      end
+    end
+    turn_count
+  end
+
+  def current_player
+    if turn_count.even?
+      "X"
+    else
+      "O"
+    end
+  end
+
+  def won?
+    WIN_COMBINATIONS.each do |win_combination|
+      # win_combination is a 3 element array of indexes that compose a win, ie [0,1,2]
+      # grab each index from the win_combination that composes a win
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+
+      position_1 = @board[win_index_1] # load the value of the board at win_index_1
+      position_2 = @board[win_index_2] # load the value of the board at win_index_2
+      position_3 = @board[win_index_3] # load the value of the board at win_index_3
+
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return win_combination
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combination
+      else
+        false
+      end
+    end
+    false
+  end
+
+  def full?
+    full_board = @board.select { |i| i != " " }
+    if full_board.length == 9
+      true # draw
+    else
+      false
+    end
+  end
+
+  def draw?
+    if full? && !won?
+      true
+    elsif won?
+      false
+    end
+  end
+
+  def over?
+    if won? || draw? || full?
+      true
+    end
+  end
+
+  def winner
+    if won?
+      win = won? # won? returns the winning array
+      @board[win[0]] # take an index from the array to show the winners token(X/O)
+    else
+      nil
+    end
+  end
+
+  def play
+    until over?
+      turn
+    end
+
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cats Game!"
+    end
+  end
+
+
 end
