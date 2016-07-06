a139ae9209f02ca1e3f50bff796b43f5a7b8ed82
Alice Wu <dragon3658aw@gmail.com>
2016-07-06 16:55:51 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,24 +22,113 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  def move(index, character)
-    @board[index - 1] = character
+  def move(position, character)
+    @board[position - 1] = character
   end
 
-  def position_taken?(index)
-    if (@board[index] == " " || @board[index] == "" || @board[index] == nil)
+  def position_taken?(position)
+    if (@board[position] == " " || @board[position] == "" || @board[position] == nil)
       return false
     else
       return true
     end
   end
 
-  def valid_move?(index)
-    if position_taken?(index.to_i)
+  def valid_move?(position)
+    if position_taken?(position.to_i - 1)
       return false
-    elsif (index.to_i < 0 || index.to_i > 8)
+    elsif (position.to_i - 1 < 0 || position.to_i - 1 > 8)
       return false
     else
       return true
     end
   end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    if valid_move?(input.to_i)
+      move(input.to_i, current_player)
+      display_board
+    else
+      turn
+    end
+  end
+
+  def turn_count
+    counter = 0
+    @board.each do |occupy|
+      if (occupy == "X" || occupy == "O")
+      counter += 1
+      end
+    end
+    return counter
+  end
+
+  def current_player
+    turn_count.even? == true ? "X" : "O"
+  end
+
+  def won?
+    WIN_COMBINATIONS.each do |win_combination|
+
+      position_1 = @board[win_combination[0]]
+      position_2 = @board[win_combination[1]]
+      position_3 = @board[win_combination[2]]
+
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return win_combination
+      end
+    end
+    return false
+  end
+
+  def full?
+    @board.all? do |index|
+      index == "X" || index == "O"
+    end
+  end
+
+  def draw?
+    if (!won? && full?)
+      return true
+    else
+      return false
+    end
+  end
+
+  def over?
+    if won? || draw? || full?
+      return true
+    end
+  end
+
+  def winner
+    WIN_COMBINATIONS.each do |win_combination|
+
+      position_1 = @board[win_combination[0]]
+      position_2 = @board[win_combination[1]]
+      position_3 = @board[win_combination[2]]
+
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X")
+        return "X"
+      elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return "O"
+      end
+    end
+    return nil
+  end
+
+  def play
+    until over?
+      turn
+      won?
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif
+      draw?
+      puts "Cats Game!"
+    end
+  end
+end
