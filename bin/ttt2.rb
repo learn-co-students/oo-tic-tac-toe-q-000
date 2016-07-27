c3014b1fa69c088cce0d77d15efb849416f0af4c
Jacques Metevier <jmetevier@gmail.com>
2016-07-27 05:16:23 +0000

    Done.

--- bin/ttt2.rb
+++ bin/ttt2.rb
@@ -18,13 +18,15 @@ class TicTacToe
     @board[@i]=token
     @token=current_player
   end
-  def position_taken?(index=@index)
-    ["X","O"].any? do |each|
-      @board[index]==each
+  def position_taken?
+    if  @board[@index]=="X" || @board[@index]=="O"
+      true
+    else
+      false
     end
   end
-  def valid_move?(input=@input)
-    @input=input.to_i
+  def valid_move?
+    @input=@input.to_i
     @index=@input-1
     false
     if !position_taken? && ([0,1,2,3,4,5,6,7,8].include? @index)
@@ -32,12 +34,14 @@ class TicTacToe
     end
   end
   def turn
-    until valid_move?
-      puts "What is your move?"
-      @input=gets.strip
+    puts "What is your move?"
+    @input=gets.strip
+    if  valid_move?
+      move
+      display_board
+    else
+      puts "invalid"
     end
-    move
-    display_board
   end
   def turn_count
     @board.count{|token| token == "X" || token == "O"}
@@ -84,14 +88,14 @@ class TicTacToe
   end
   def play
     @token="X"
-    until over? || draw? || won?
+    until won? or draw?
       turn
     end
     winner
     if won? && @winner=="X"
       puts "Congratulations X!"
     elsif won? && @winner=="O"
-      puts "Congratulations O!"
+      puts "Congragulations O!"
     else draw?
       puts "Cats Game!"
     end
