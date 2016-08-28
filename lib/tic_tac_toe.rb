a0e807e7690a2a3f5127b2147bd44f720444a988
Brett Cole <michael.4486@gmail.com>
2016-08-28 00:27:39 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,6 +32,80 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    index.to_i.between?(0,8) && !position_taken?(index.to_i)
+    index = index.to_i - 1
+    index.between?(0,8) && !position_taken?(index.to_i)
   end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input.to_i
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      turn
+    end
+  end
+
+  def turn_count
+    @board.count {|token| token == "X"|| token == "O"}
+  end
+
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+
+  def won?
+
+    WIN_COMBINATIONS.each do |win_combo|
+      if (@board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]] == "X") || (@board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O")
+        return win_combo
+      end
+    end
+    return false
+  end
+
+  #ternary syntax
+  def full?
+    @board.all? {|full| full == "X" || full == "O"} ? true : false
+  end
+
+  def draw?
+    if full? == true && won?  == false
+      return true
+    else
+      return false
+    end
+  end
+
+  def over?
+    if draw? == true || full? == true || won? == true
+      return true
+    else
+      false
+    end
+  end
+
+  def winner
+    return nil if !won?
+    position = won?[0]
+      if @board[position] == "X"
+        return "X"
+      elsif @board[position] == "O"
+        return "O"
+    end
+  end
+
+  def play
+    until over?
+      turn
+    end
+      if won?
+        puts "Congratulations #{winner}!"
+      elsif draw?
+        puts "Cats Game!"
+      end
+    end
+
 end
